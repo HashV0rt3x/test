4e19649f0c211e5aa64672359262fbf578d49111
Neo <neo@Neos-MacBook-Pro.local>
2026-02-20 17:52:05 +0500

    master

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Telegram Mini App initialization
+const tg = (window as any).Telegram?.WebApp
+if (tg) {
+  tg.ready()
+  tg.expand() // Full screen ochish
+  tg.setHeaderColor('#0f0f13')
+  tg.setBackgroundColor('#0f0f13')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
